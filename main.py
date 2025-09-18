813db2d97389fb0cf83b33e2c2961c7bb31b339b
Giovanni Sanchez <giosr295@gmail.com>
2025-09-18 17:45:29 -0600

    Cuarto commit

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Hola')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+optio
